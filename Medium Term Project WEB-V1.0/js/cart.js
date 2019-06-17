84bd5c17907dbfdacc5b61cf72b80365215e9ffc
Tedu <andor_song@foxmail.com>
2019-06-17 21:37:38 +0800

    修改 register.html

--- Medium Term Project WEB-V1.0/js/cart.js
+++ Medium Term Project WEB-V1.0/js/cart.js
@@ -1,4 +1,11 @@
 $(function (){
+	$(".togg").click(function(){
+		if($(".TeamServices").css("display") == "none"){
+			$(".TeamServices").css("display","block");
+		}else{
+			$(".TeamServices").css("display","none");
+		}
+	})
 	$(".checkAll").click(function (){
 		if($(this).attr("checked")){
 			$(this).removeAttr("checked").attr("src","./images/product_normal.png");
