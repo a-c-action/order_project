5d9bd5f97379b06d10050f3abc598b3c16aefcb0
Tedu <1773298741@qq.con>
2019-06-21 18:53:46 +0800

    bug

--- Medium Term Project WEB-V1.0/js/order.js
+++ Medium Term Project WEB-V1.0/js/order.js
@@ -1,5 +1,6 @@
-$(".contentorder .ordera").click(function (){
+$(function(){
+    $(".ordera").click(function (){
 		//移除整个商品记录
-		console.log($(".ordera"))
 		$(this).parents(".contentorder").remove();
 	})
+})
